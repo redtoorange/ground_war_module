c6607fde2b32552d71ef8c600243ad087540a1c0
Andrew McGuiness <redtoorange@gmail.com>
2021-12-23 08:08:05 -0500

    Added base unit

---
+++ BaseUnit.h
@@ -0,0 +1,45 @@
+﻿#ifndef BASEUNIT_H
+#define BASEUNIT_H
+#include "scene/2d/node_2d.h"
+#include "scene/2d/sprite.h"
+
+class BaseUnit : public Node2D  {
+	GDCLASS(BaseUnit, Node2D);
+public:
+	enum SelectionState {
+		UNSELECTED,
+		SELECTED
+	};
+
+	enum HoverState {
+		UNHOVERED,
+		HOVERED
+	};
+
+	BaseUnit();
+
+
+protected:
+	static void _bind_methods();
+	void _notification(int p_what);
+	void _ready();
+
+	void _process(float delta);
+	void _unhandled_input(Ref<InputEvent> p_event);
+
+private:
+	void UpdateOverlays();
+
+	void OnMouseEntered();
+	void OnMouseExited();
+
+	Sprite* unitSprite;
+	Sprite* selectionSprite;
+	Sprite* hoveredSprite;
+
+	HoverState currentHoverState;
+	SelectionState currentSelectionState;
+};
+
+#endif
+
--- register_types.cpp
+++ register_types.cpp
@@ -1,6 +1,9 @@
 #include "register_types.h"
 
+#include "BaseUnit.h"
+
 void register_ground_war_types() {
+	ClassDB::register_class<BaseUnit>();
 }
 
 void unregister_ground_war_types() {
